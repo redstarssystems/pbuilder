7cc389dfe5eee3f8475e586271959d698ab47e0d
Mike Ananev <mike1452@yandex.ru>
2020-08-15 21:51:03 +0300

    deleted files

--- test-projects/javac/src/clj/org/rssys/java_exmpl/core.clj
+++
@@ -1,7 +0,0 @@
-(ns org.rssys.java-exmpl.core
-  (:import [org.rssys.javac_example Hello] ))
-
-(defn  -main [& args]
-  (println "running main")
-  (Hello/sayHello "Java world!")
-  )
--- test-projects/javac/src/java/org/rssys/javac_example/Hello.java
+++
@@ -1,7 +0,0 @@
-package org.rssys.javac_example;
-
-public class Hello {
-  public static void sayHello(String name){
-    System.out.println("Hello, " + name);
-  }
-}
